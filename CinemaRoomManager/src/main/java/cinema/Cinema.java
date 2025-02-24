f9a898ebcd4a087bce257792bc2f108479e18c9a
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-02-24 17:22:03 -0500

    Cinema Room Manager: complete Stage 1

--- CinemaRoomManager/src/main/java/cinema/Cinema.java
+++ CinemaRoomManager/src/main/java/cinema/Cinema.java
@@ -1,8 +1,22 @@
 package cinema;
 
+import java.util.stream.IntStream;
+
 public class Cinema {
+  public static final int ROWS = 7;
+  public static final int SEATS_PER_ROW = 8;
 
   public static void main(String[] args) {
-    System.out.println("Cinema Room Manager");
+    StringBuilder headerRowBuilder = new StringBuilder();
+    IntStream.rangeClosed(1, SEATS_PER_ROW).forEach(i -> headerRowBuilder.append(" " + i));
+    String headerRow = headerRowBuilder.toString();
+
+    StringBuilder rowBuilder = new StringBuilder();
+    IntStream.rangeClosed(1, SEATS_PER_ROW).forEach(i -> rowBuilder.append(" S"));
+    String row = rowBuilder.toString();
+
+    System.out.println("Cinema:");
+    System.out.println(" " + headerRow);
+    IntStream.rangeClosed(1, ROWS).forEach(i -> System.out.println(i + row));
   }
 }

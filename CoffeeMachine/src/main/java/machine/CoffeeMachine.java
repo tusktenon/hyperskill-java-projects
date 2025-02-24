353da4d23ca8e25549557ad90de3e63733d7659d
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-02-24 13:56:04 -0500

    Coffee Machine Stage 1: refactor
    Instead of declaring STEPS as an array and then converting it to a list
    to access the `forEach` method, just make it a list in the first place.

--- CoffeeMachine/src/main/java/machine/CoffeeMachine.java
+++ CoffeeMachine/src/main/java/machine/CoffeeMachine.java
@@ -1,19 +1,19 @@
 package machine;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class CoffeeMachine {
-  public static final String[] STEPS = {
-    "Starting to make a coffee",
-    "Grinding coffee beans",
-    "Boiling water",
-    "Mixing boiled water with crushed coffee beans",
-    "Pouring coffee into the cup",
-    "Pouring some milk into the cup",
-    "Coffee is ready!"
-  };
+  public static final List<String> STEPS =
+      List.of(
+          "Starting to make a coffee",
+          "Grinding coffee beans",
+          "Boiling water",
+          "Mixing boiled water with crushed coffee beans",
+          "Pouring coffee into the cup",
+          "Pouring some milk into the cup",
+          "Coffee is ready!");
 
   public static void main(String[] args) {
-    Arrays.asList(STEPS).forEach(System.out::println);
+    STEPS.forEach(System.out::println);
   }
 }

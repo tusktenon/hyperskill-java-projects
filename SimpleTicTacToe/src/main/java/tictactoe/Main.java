07132a616ed1352f60b0f82e6173f9790ed38bcc
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-03-01 21:38:08 -0500

    Simple Tic-Tac-Toe: complete Stage 1

--- SimpleTicTacToe/src/main/java/tictactoe/Main.java
+++ SimpleTicTacToe/src/main/java/tictactoe/Main.java
@@ -2,6 +2,11 @@ package tictactoe;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Simple Tic-Tac-Toe");
+    System.out.println(
+        """
+        X O X
+        O X O
+        X X O
+        """);
   }
 }

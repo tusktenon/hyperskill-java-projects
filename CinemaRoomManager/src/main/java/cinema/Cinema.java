1444ad7af8a76816c47b73a30f9e61cbe17c750c
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-03-04 08:24:36 -0500

    Cinema Room Manager: complete Stage 3

--- CinemaRoomManager/src/main/java/cinema/Cinema.java
+++ CinemaRoomManager/src/main/java/cinema/Cinema.java
@@ -1,23 +1,65 @@
 package cinema;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Cinema {
+  private final int rows, seatsPerRow;
+  private int reservedRow, reservedSeat;
+
+  Cinema(int rows, int seatsPerRow) {
+    this.rows = rows;
+    this.seatsPerRow = seatsPerRow;
+  }
+
+  void reserve(int row, int seat) {
+    reservedRow = row;
+    reservedSeat = seat;
+  }
+
+  String seatingChart() {
+    StringBuilder chart = new StringBuilder(" ");
+    IntStream.rangeClosed(1, seatsPerRow).forEach(i -> chart.append(" " + i));
+    IntStream.rangeClosed(1, rows)
+        .forEach(
+            i -> {
+              chart.append("\n" + i);
+              if (i == reservedRow) {
+                chart.append(
+                    " S".repeat(reservedSeat - 1) + " B" + " S".repeat(seatsPerRow - reservedSeat));
+              } else {
+                chart.append(" S".repeat(seatsPerRow));
+              }
+            });
+    return chart.toString();
+  }
+
+  int ticketPrice() {
+    return rows * seatsPerRow > 60 && reservedRow > rows / 2 ? 8 : 10;
+  }
 
   public static void main(String[] args) {
     try (Scanner in = new Scanner(System.in)) {
-      System.out.println("Enter the number of rows:");
-      System.out.print("> ");
+      System.out.print("Enter the number of rows:\n> ");
       int rows = in.nextInt();
 
-      System.out.println("Enter the number of seats in each row:");
-      System.out.print("> ");
-      int seats = in.nextInt();
+      System.out.print("Enter the number of seats in each row:\n> ");
+      int seatsPerRow = in.nextInt();
 
-      int profit =
-          rows * seats > 60 ? ((rows / 2) * 10 + (rows - rows / 2) * 8) * seats : rows * seats * 10;
-      System.out.println("Total income:");
-      System.out.println("$" + profit);
+      Cinema cinema = new Cinema(rows, seatsPerRow);
+      System.out.println("\nCinema:");
+      System.out.println(cinema.seatingChart());
+
+      System.out.print("\nEnter a row number:\n> ");
+      int row = in.nextInt();
+
+      System.out.print("Enter a seat number in that row:\n> ");
+      int seat = in.nextInt();
+
+      cinema.reserve(row, seat);
+      System.out.println("\nTicket price: $" + cinema.ticketPrice());
+      System.out.println("\nCinema:");
+      System.out.println(cinema.seatingChart());
     }
   }
 }

90698ad0b992908e50244bf65b7b4c470b8e75a7
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-03-04 09:35:03 -0500

    Cinema Room Manager: complete Stage 4

--- CinemaRoomManager/src/main/java/cinema/Cinema.java
+++ CinemaRoomManager/src/main/java/cinema/Cinema.java
@@ -1,41 +1,81 @@
 package cinema;
 
+import java.util.BitSet;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Cinema {
+  private static final String menu =
+      """
+
+      1. Show the seats
+      2. Buy a ticket
+      0. Exit
+      > \
+      """;
+
   private final int rows, seatsPerRow;
-  private int reservedRow, reservedSeat;
+  private final BitSet reserved;
 
   Cinema(int rows, int seatsPerRow) {
     this.rows = rows;
     this.seatsPerRow = seatsPerRow;
+    reserved = new BitSet(rows * seatsPerRow);
   }
 
-  void reserve(int row, int seat) {
-    reservedRow = row;
-    reservedSeat = seat;
+  void run(Scanner in) {
+    while (true) {
+      System.out.print(menu);
+      try {
+        int selection = in.nextInt();
+        switch (selection) {
+          case 0 -> {
+            return;
+          }
+          case 1 -> System.out.println(seatingChart());
+          case 2 -> {
+            System.out.print("\nEnter a row number:\n> ");
+            int row = in.nextInt();
+
+            System.out.print("Enter a seat number in that row:\n> ");
+            int seat = in.nextInt();
+
+            reserve(row, seat);
+            System.out.println("\nTicket price: $" + ticketPrice(row));
+          }
+          default -> System.out.println("Invalid selection");
+        }
+      } catch (NoSuchElementException e) {
+        in.nextLine(); // clear input buffer
+        System.out.println("Invalid selection");
+      }
+    }
+  }
+
+  private void reserve(int row, int seat) {
+    reserved.set((row - 1) * seatsPerRow + (seat - 1));
+  }
+
+  private boolean isReserved(int row, int seat) {
+    return reserved.get((row - 1) * seatsPerRow + (seat - 1));
   }
 
   String seatingChart() {
-    StringBuilder chart = new StringBuilder(" ");
+    StringBuilder chart = new StringBuilder("\nCinema:\n ");
     IntStream.rangeClosed(1, seatsPerRow).forEach(i -> chart.append(" " + i));
     IntStream.rangeClosed(1, rows)
         .forEach(
             i -> {
               chart.append("\n" + i);
-              if (i == reservedRow) {
-                chart.append(
-                    " S".repeat(reservedSeat - 1) + " B" + " S".repeat(seatsPerRow - reservedSeat));
-              } else {
-                chart.append(" S".repeat(seatsPerRow));
-              }
+              IntStream.rangeClosed(1, seatsPerRow)
+                  .forEach(j -> chart.append(isReserved(i, j) ? " B" : " S"));
             });
     return chart.toString();
   }
 
-  int ticketPrice() {
-    return rows * seatsPerRow > 60 && reservedRow > rows / 2 ? 8 : 10;
+  int ticketPrice(int row) {
+    return rows * seatsPerRow > 60 && row > rows / 2 ? 8 : 10;
   }
 
   public static void main(String[] args) {
@@ -47,19 +87,7 @@ public class Cinema {
       int seatsPerRow = in.nextInt();
 
       Cinema cinema = new Cinema(rows, seatsPerRow);
-      System.out.println("\nCinema:");
-      System.out.println(cinema.seatingChart());
-
-      System.out.print("\nEnter a row number:\n> ");
-      int row = in.nextInt();
-
-      System.out.print("Enter a seat number in that row:\n> ");
-      int seat = in.nextInt();
-
-      cinema.reserve(row, seat);
-      System.out.println("\nTicket price: $" + cinema.ticketPrice());
-      System.out.println("\nCinema:");
-      System.out.println(cinema.seatingChart());
+      cinema.run(in);
     }
   }
 }

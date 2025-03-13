6e54bd1eb610dde1e5647d30cded0b375f137552
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-03-13 12:25:17 -0400

    Traffic Light: complete Stage 1

--- TrafficLight/src/main/java/traffic/Main.java
+++ TrafficLight/src/main/java/traffic/Main.java
@@ -1,7 +1,17 @@
 package traffic;
 
 public class Main {
+    private static final String greeting = "Welcome to the traffic management system!";
+    private static final String menu = """
+            Menu:
+            1. Add
+            2. Delete
+            3. System
+            0. Quit
+            """;
+
     public static void main(String[] args) {
-        System.out.println("Traffic Light Simulator");
+        System.out.println(greeting);
+        System.out.println(menu);
     }
 }

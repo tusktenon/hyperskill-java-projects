dafed93ff8759a13a4e63ac9102abf4f08cc362b
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-03-18 14:40:48 -0400

    Budget Manager: complete Stage 1

--- BudgetManager/src/main/java/budget/Main.java
+++ BudgetManager/src/main/java/budget/Main.java
@@ -1,7 +1,30 @@
 package budget;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private static List<String> purchases = new ArrayList<>();
+
+    private static double getAmount(String purchase) {
+        int dollarIndex = purchase.indexOf('$');
+        String amountString = purchase.substring(dollarIndex + 1);
+        return Double.parseDouble(amountString);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Budget Manager");
+        try (Scanner in = new Scanner(System.in)) {
+            while (in.hasNextLine()) {
+                purchases.add(in.nextLine());
+            }
+        }
+
+        double total = 0;
+        for (String purchase : purchases) {
+            System.out.println(purchase);
+            total += getAmount(purchase);
+        }
+        System.out.printf("Total: $%.2f\n", total);
     }
 }

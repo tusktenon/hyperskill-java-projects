c11072ece5fe040c2db0a1a63923aa1971f378a9
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-03-31 21:22:00 -0400

    Learning Tracker: complete Stage 1

--- LearningProgressTracker/src/main/java/tracker/Main.java
+++ LearningProgressTracker/src/main/java/tracker/Main.java
@@ -1,8 +1,23 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Learning Progress Tracker");
+
+        try (Scanner in = new Scanner(System.in)) {
+            while (true) {
+                switch (in.nextLine().trim().toLowerCase()) {
+                    case "exit" -> {
+                        System.out.println("Bye!");
+                        return;
+                    }
+                    case "" -> System.out.println("No input.");
+                    default -> System.out.println("Unknown command!");
+                }
+            }
+        }
     }
 }

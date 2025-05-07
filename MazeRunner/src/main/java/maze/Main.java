9477d7e426cad8e37ebf115badb72f8c69cc40ce
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-05-07 11:07:43 -0400

    Maze Runner: complete Stage 1

--- MazeRunner/src/main/java/maze/Main.java
+++ MazeRunner/src/main/java/maze/Main.java
@@ -2,7 +2,28 @@ package maze;
 
 public class Main {
 
+    public static final byte[][] MAZE = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {0, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+            {1, 0, 1, 0, 0, 0, 1, 0, 1, 1},
+            {1, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 1},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 1},
+            {1, 0, 1, 0, 1, 0, 0, 0, 1, 1},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 1},
+            {1, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    };
+
+    public static final String PASSAGE = "  ";
+    public static final String WALL = "██";
+
     public static void main(String[] args) {
-        System.out.println("Maze Runner");
+        for (var row : MAZE) {
+            for (var cell : row) {
+                System.out.print(cell == 1 ? WALL : PASSAGE);
+            }
+            System.out.println();
+        }
     }
 }

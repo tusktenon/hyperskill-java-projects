23c41d7000d07a14ab893ca5ccac082a0d09fcb0
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2025-05-18 16:58:19 -0400

    QRCode Service: complete Stage 2

--- QRCodeService/src/main/java/qrcodeapi/Controller.java
+++ QRCodeService/src/main/java/qrcodeapi/Controller.java
@@ -1,19 +1,34 @@
 package qrcodeapi;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
+
+    private static final BufferedImage BLANK;
+
+    static {
+        BLANK = new BufferedImage(250, 250, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = BLANK.createGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, 250, 250);
+    }
 
     @GetMapping("/health")
     @ResponseStatus(HttpStatus.OK)
     public void health() {
     }
 
-    @GetMapping("/qrcode")
-    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    public void qrcode() {
+    @GetMapping(path = "/qrcode")
+    public ResponseEntity<BufferedImage> qrcode() {
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(BLANK);
     }
 }

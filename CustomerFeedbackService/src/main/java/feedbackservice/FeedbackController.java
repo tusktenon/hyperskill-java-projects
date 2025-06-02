22890526fa8d04e11b32e03d87b1fc30aba34d85
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2025-06-02 15:41:19 -0400

    CFS: complete Stage 2

--- CustomerFeedbackService/src/main/java/feedbackservice/FeedbackController.java
+++ CustomerFeedbackService/src/main/java/feedbackservice/FeedbackController.java
@@ -1,10 +1,14 @@
 package feedbackservice;
 
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class FeedbackController {
@@ -13,6 +17,17 @@ public class FeedbackController {
 
     public FeedbackController(FeedbackRepository repository) {
         this.repository = repository;
+    }
+
+    @GetMapping("/feedback")
+    public List<Feedback> getAllFeedback() {
+        return repository.findAll(Sort.by("id").descending());
+    }
+
+    @GetMapping("/feedback/{id}")
+    public Feedback getFeedbackById(@PathVariable String id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/feedback")

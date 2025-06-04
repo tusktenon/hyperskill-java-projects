6bf90d9562986fdf7d9b89c048402c35952e0ae0
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2025-06-03 21:05:11 -0400

    Transaction Aggregator: complete Stage 1

--- TransactionAggregator/src/main/java/aggregator/Application.java
+++ TransactionAggregator/src/main/java/aggregator/Application.java
@@ -4,12 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/aggregate")
+    public String aggregate() {
+        // As of Spring 6.1, RestClient is preferred to RestTemplate
+        return RestClient.create("http://localhost:8889/ping")
+                .get()
+                .retrieve()
+                .body(String.class);
     }
 }

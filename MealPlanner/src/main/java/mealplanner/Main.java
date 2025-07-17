27f5e608ad418cc9056339d4cb1918547c18064e
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2025-07-16 20:09:43 -0400

    Meal Planner: complete Stage 2

--- MealPlanner/src/main/java/mealplanner/Main.java
+++ MealPlanner/src/main/java/mealplanner/Main.java
@@ -1,26 +1,12 @@
 package mealplanner;
 
+import java.util.ArrayList;
 import java.util.Scanner;
-
-import static mealplanner.Meal.Category;
 
 public class Main {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
-            System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
-            Category category = Category.valueOf(in.nextLine());
-
-            System.out.println("Input the meal's name:");
-            String name = in.nextLine();
-
-            System.out.println("Input the ingredients:");
-            String[] ingredients = in.nextLine().split(",");
-
-            Meal meal = new Meal(category, name, ingredients);
-
-            System.out.println();
-            System.out.println(meal);
-            System.out.println("The meal has been added!");
+            new Menu(in, new ArrayList<>()).run();
         }
     }
 }
---
+++ MealPlanner/src/main/java/mealplanner/Menu.java
@@ -0,0 +1,93 @@
+package mealplanner;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+import static mealplanner.Meal.Category;
+
+public class Menu {
+
+    private static final Pattern VALID_NAME = Pattern.compile("[A-Za-z ]+");
+
+    private final Scanner in;
+    private final List<Meal> meals;
+
+    public Menu(Scanner in, List<Meal> meals) {
+        this.in = in;
+        this.meals = meals;
+    }
+
+    public void run() {
+        while (true) {
+            System.out.println("What would you like to do (add, show, exit)?");
+            switch (in.nextLine()) {
+                case "add" -> addMeal();
+                case "show" -> displayMeals();
+                case "exit" -> {
+                    System.out.println("Bye!");
+                    return;
+                }
+            }
+        }
+    }
+
+    private void addMeal() {
+        Category category = getCategory();
+        String name = getName();
+        String[] ingredients = getIngredients();
+        meals.add(new Meal(category, name, ingredients));
+        System.out.println("The meal has been added!");
+    }
+
+    private Category getCategory() {
+        System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
+        while (true) {
+            try {
+                return Category.valueOf(in.nextLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
+            }
+        }
+    }
+
+    private String getName() {
+        System.out.println("Input the meal's name:");
+        while (true) {
+            try {
+                return trimAndValidate(in.nextLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Wrong format. Use letters only!");
+            }
+        }
+    }
+
+    private String[] getIngredients() {
+        System.out.println("Input the ingredients:");
+        while (true) {
+            try {
+                return Arrays.stream(in.nextLine().split(","))
+                        .map(Menu::trimAndValidate)
+                        .toArray(String[]::new);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Wrong format. Use letters only!");
+            }
+        }
+    }
+
+    private static String trimAndValidate(String input) {
+        input = input.trim();
+        if (!VALID_NAME.matcher(input).matches()) {
+            throw new IllegalArgumentException();
+        }
+        return input;
+    }
+
+    private void displayMeals() {
+        if (meals.isEmpty()) {
+            System.out.println("No meals saved. Add a meal first.");
+        } else {
+            meals.forEach(meal -> System.out.printf("%n%s%n", meal));
+            System.out.println();
+        }
+    }
+}

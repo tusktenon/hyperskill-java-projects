b99872377958117879c4b351ad39f61efaf594a8
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2025-07-26 10:20:34 -0400

    JSON Database: add Stage 5 file functionality
    A partial solution for Stage 5: the client can read requests from files,
    and the server can persist the database as a file (i.e., load from and
    save to a file), but the server still handles request sequentially.

--- JSONDatabase/src/main/java/client/Main.java
+++ JSONDatabase/src/main/java/client/Main.java
@@ -7,13 +7,16 @@ import shared.Request;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
 
     private static final String ADDRESS = "127.0.0.1";
     private static final int PORT = 23456;
+    private static final String REQUESTS_DIR = "src/client/data/";
 
-    @Parameter(names = "-t", required = true, description = "Request type")
+    @Parameter(names = "-t", description = "Request type")
     Request.Type type;
 
     @Parameter(names = "-k", description = "Key")
@@ -21,6 +24,9 @@ public class Main {
 
     @Parameter(names = "-v", description = "Value")
     String value;
+
+    @Parameter(names = "-in", description = "Request file")
+    String fileName;
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
@@ -33,7 +39,10 @@ public class Main {
              DataInputStream input = new DataInputStream(socket.getInputStream());
              DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
             System.out.println("Client started!");
-            String requestJson = new Gson().toJson(new Request(type, key, value));
+            String requestJson = fileName == null
+                    ? new Gson().toJson(new Request(type, key, value))
+                    : Files.readString(Path.of(
+                    System.getProperty("user.dir"), REQUESTS_DIR, fileName));
             output.writeUTF(requestJson);
             System.out.println("Sent: " + requestJson);
             System.out.println("Received: " + input.readUTF());
--- JSONDatabase/src/main/java/server/Main.java
+++ JSONDatabase/src/main/java/server/Main.java
@@ -6,34 +6,42 @@ import shared.Response;
 
 import java.io.*;
 import java.net.*;
+import java.nio.file.*;
 
 public class Main {
 
     private static final String ADDRESS = "127.0.0.1";
     private static final int PORT = 23456;
+    private static final String DATA_FILE = "src/server/data/db.json";
 
-    private static final JsonObject database = new JsonObject();
+    private static final Path dataFilePath = Path.of(System.getProperty("user.dir"), DATA_FILE);
+    private static JsonObject database;
 
     public static void main(String[] args) throws IOException {
+        database = new Gson().fromJson(Files.readString(dataFilePath), JsonObject.class);
         try (ServerSocket server = new ServerSocket(PORT, 50, InetAddress.getByName(ADDRESS))) {
             System.out.println("Server started!");
-            while (true) {
-                try (Socket socket = server.accept();
-                     DataInputStream input = new DataInputStream(socket.getInputStream());
-                     DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
-                    String received = input.readUTF();
-                    System.out.println("Received: " + received);
-                    Request request = new Gson().fromJson(received, Request.class);
-                    String responseJson = new Gson().toJson(processRequest(request));
-                    output.writeUTF(responseJson);
-                    System.out.println("Sent: " + responseJson);
-                    if (request.type() == Request.Type.exit) break;
-                }
+            listen(server);
+        }
+    }
+
+    private static void listen(ServerSocket server) throws IOException {
+        while (true) {
+            try (Socket socket = server.accept();
+                 DataInputStream input = new DataInputStream(socket.getInputStream());
+                 DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+                String received = input.readUTF();
+                System.out.println("Received: " + received);
+                Request request = new Gson().fromJson(received, Request.class);
+                String responseJson = new Gson().toJson(processRequest(request));
+                output.writeUTF(responseJson);
+                System.out.println("Sent: " + responseJson);
+                if (request.type() == Request.Type.exit) break;
             }
         }
     }
 
-    private static Response processRequest(Request request) {
+    private static Response processRequest(Request request) throws IOException {
         switch (request.type()) {
             case get -> {
                 JsonElement value = database.get(request.key());
@@ -43,15 +51,26 @@ public class Main {
             }
             case set -> {
                 database.addProperty(request.key(), request.value());
+                writeDatabase();
                 return Response.ok();
             }
             case delete -> {
                 JsonElement deleted = database.remove(request.key());
-                return deleted == null ? Response.err("No such key") : Response.ok();
+                if (deleted == null) {
+                    return Response.err("No such key");
+                } else {
+                    writeDatabase();
+                    return Response.ok();
+                }
             }
             default -> { // case exit
                 return Response.ok();
             }
         }
+    }
+
+    private static void writeDatabase() throws IOException {
+        String contents = new Gson().toJson(database);
+        Files.writeString(dataFilePath, contents, StandardOpenOption.TRUNCATE_EXISTING);
     }
 }

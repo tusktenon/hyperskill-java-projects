698f86b674e58977d509a5b3f3226f84c16d9b11
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2025-08-22 16:44:23 -0400

    Web Quiz Engine: move UserDetailsService impl.
    Instead of a named class, it's a little tidier to place the
    UserDetailsService implementation in SecurityConfig as a bean, defined
    with a lambda expression.

--- WebQuizEngine/src/main/java/engine/SecurityConfig.java
+++ WebQuizEngine/src/main/java/engine/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,11 +16,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable) // for POST requests via Postman
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/actuator/shutdown").permitAll() // required for testing
+                        .requestMatchers(HttpMethod.POST, "/actuator/shutdown").permitAll() //
+                        // required for testing
                         .requestMatchers(HttpMethod.POST, "/api/register").permitAll()
                         .requestMatchers("/api/quizzes/**").authenticated()
                         .anyRequest().denyAll()
@@ -27,7 +35,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public UserDetailsService userDetailsService(AppUserRepository repository) {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
     }
 }

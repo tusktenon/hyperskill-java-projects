1250e3cf979c152e8aa20f9c12165fd0bc369ef2
Dan Murphy <70486892+tusktenon@users.noreply.github.com>
2025-10-09 13:29:57 -0400

    TMS: clean up inner interfaces in Views
    An interface nested inside an outer interface is necessarily public and
    static.

--- TaskManagementSystem/src/main/java/taskmanagement/presentation/Views.java
+++ TaskManagementSystem/src/main/java/taskmanagement/presentation/Views.java
@@ -1,8 +1,8 @@
 package taskmanagement.presentation;
 
 public interface Views {
-    public static interface TaskViews {
-        public static interface WithoutCommentCount {}
-        public static interface WithCommentCount extends WithoutCommentCount {}
+    interface TaskViews {
+        interface WithoutCommentCount {}
+        interface WithCommentCount extends WithoutCommentCount {}
     }
 }
